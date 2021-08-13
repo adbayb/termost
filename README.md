6af00b6cb604c065970c7001aba2bcc03f930e60
Ayoub Adib <adbayb@gmail.com>
2021-08-14 01:07:54 +0200

    chore: update readme and todo

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # termost
 
 Get the most of your terminal 💻
+
+## Roadmap
+
+-   [x] API
+-   [x] Examples
+-   [ ] Test
+-   [ ] Documentation
--- example/index.ts
+++ example/index.ts
@@ -1,6 +1,8 @@
 import { termost } from "../src";
 
 // @todo: setup() helper called inside termost to handle global error listeners?
+// @todo: add function to generate label with values for `question` and `task` api
+// @todo: typing for values `termost<{ ls: string }>()`
 
 const program = termost(
 	"Program description placeholder. Program name and version are retrieved from your `package.json`. You can override this automatic retrieval by using the `termost({ name, description, version })` builder form."
