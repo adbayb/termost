3a2c0088341a8293c16a875f9b8fd334672f3401
Ayoub Adib <adbayb@gmail.com>
2021-11-22 22:04:43 +0100

    feat: expose helpers API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import { exec } from "./core/process";
+import { format, print } from "./features/message/helpers";
+
+export const helpers = {
+	exec,
+	format,
+	print,
+};
+
 export { termost } from "./termost";
 
 export type { Termost } from "./termost";
